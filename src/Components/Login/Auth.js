b98ac4c3a88be62c9e57c1c9c7910ab8fe9b2ec5
Sofía Maturana <sofimatsu@gmail.com>
2018-10-03 08:40:58 -0300

    auth

--- src/Components/Login/Auth.js
+++ src/Components/Login/Auth.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import firebaseIntegration from '../../firebase';
+import withFirebaseAuth from 'react-auth-firebase';
 
 
+class Auth extends Component {
+    render() {
+
+        const {
+            signInWithGoogle,
+            signInWithFacebook,
+            googleAccessToken,
+            facebookAccessToken,
+            signOut,
+            user,
+            error
+        } = this.props;
+
+        return ( 
+            <div>
+                <button onClick = { signInWithGoogle }> Signin with Google </button> 
+            <button onClick = { signInWithFacebook } > Signin with Facebook </button>
+            </div>
+
+        )
+    }
+
+}
+
+const authConfig = {
+    google: {
+        redirect: true,
+        returnAccessToken: true,
+        saveUserInDatabase: true
+    },
+
+    facebook: {
+        redirect: true,
+        returnAccessToken: true,
+        saveUserInDatabase: true
+    }
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default Auth;
+export default withFirebaseAuth(Auth, firebase, authConfig);
